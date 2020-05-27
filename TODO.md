ebb0c2b65a7bbfc15d49a2fc025c5ae0dd0d1383
Cesar Castillo <ceaucari@gmail.com>
2020-05-27 03:17:14 -0500

    Server working with apollo and User and Message resolvers

---
+++ TODO.md
@@ -0,0 +1,1 @@
+- add ESLint unused rules
---
+++ src/models/index.js
@@ -0,0 +1,30 @@
+let users = {
+  1: {
+    id: '1',
+    username: 'Cesar Castillo',
+    messageIds: [1],
+  },
+  2: {
+    id: '2',
+    username: 'John Doe',
+    messageIds: [2],
+  },
+};
+
+let messages = {
+  1: {
+    id: '1',
+    text: 'Hello World',
+    userId: '1',
+  },
+  2: {
+    id: '2',
+    text: 'By World',
+    userId: '2',
+  },
+};
+
+export default {
+  users,
+  messages,
+};
